28a5d475cbf86c84cf78f201a39b33e6641d04a6
slieman <slieman2001@hotmail.com>
2020-03-25 14:08:15 +0200

    working with localStorage, seesionStorage, IndexDb, Cookies, CacheStorage

--- app.js
+++ app.js
@@ -1,17 +1,58 @@
 const storeBtn = document.getElementById('store-btn');
 const retrBtn = document.getElementById('retrieve-btn');
 
+let db;
+
+const dbRequest = indexedDB.open('StorageDummy', 1);
+
+dbRequest.onsuccess = function(event) {
+  db = event.target.result;
+};
+
+dbRequest.onupgradeneeded = function(event) {
+  db = event.target.result;
+
+  const objStore = db.createObjectStore('products', { keyPath: 'id' });
+
+  objStore.transaction.oncomplete = function(event) {
+    const productsStore = db
+      .transaction('products', 'readwrite')
+      .objectStore('products');
+    productsStore.add({
+      id: 'p1',
+      title: 'A First Product',
+      price: 12.99,
+      tags: ['Expensive', 'Luxury']
+    });
+  };
+};
+
+dbRequest.onerror = function(event) {
+  console.log('ERROR!');
+};
+
 storeBtn.addEventListener('click', () => {
-    const userId= 'u123';
-    const user = {name:'Max', age:30}
-   document.cookie= `uid=${userId} max-age=360`;
-   document.cookie = `user=${JSON.stringify(user)}`
+  if (!db) {
+    return;
+  }
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  productsStore.add({
+    id: 'p2',
+    title: 'A Second Product',
+    price: 122.99,
+    tags: ['Expensive', 'Luxury']
+  });
 });
+
 retrBtn.addEventListener('click', () => {
-    console.log(document.cookie);
-    const cookieData = document.cookie.split(';');
-    const data = cookieData.map(i=>{
-        return i.trim();
-    })
-    console.log(data[1].split('=')[1]); //userValue
+  const productsStore = db
+    .transaction('products', 'readwrite')
+    .objectStore('products');
+  const request = productsStore.get('p2');
+
+  request.onsuccess = function() {
+    console.log(request.result);
+  }
 });
